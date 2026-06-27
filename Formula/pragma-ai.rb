7a32bbc72777538f8890eb8c82abfe7817c7ffa0
Azure DevOps Pipeline <pipeline@pragma.com.co>
2026-06-27 07:17:49 +0000

    chore: release pragma-ai v0.1.4 (dev)

--- Formula/pragma-ai.rb
+++ Formula/pragma-ai.rb
@@ -1,27 +1,27 @@
 class PragmaAi < Formula
   desc "Pragma AI CLI — sync AI assistant configuration for Pragma projects"
   homepage "https://github.com/somospragma/pragma-ai-cli-formula"
-  version "0.1.3"
+  version "0.1.4"
 
   on_macos do
     on_arm do
-      url "https://registry-dev.pragma.com.co/repository/pragma-raw-dev-releases/pragma-ai-cli/0.1.3/pragma-ai-cli_0.1.3_darwin_arm64.tar.gz"
-      sha256 "d8ff251557dda5edf5ec1e609dc2b795944ffe610af74f114ca04b314473e670"
+      url "https://registry-dev.pragma.com.co/repository/pragma-raw-dev-releases/pragma-ai-cli/0.1.4/pragma-ai-cli_0.1.4_darwin_arm64.tar.gz"
+      sha256 "45023ec9b6775c35bfc1349e4796b508e3143e2c142ffb8c1eba4a06104eee86"
     end
     on_intel do
-      url "https://registry-dev.pragma.com.co/repository/pragma-raw-dev-releases/pragma-ai-cli/0.1.3/pragma-ai-cli_0.1.3_darwin_amd64.tar.gz"
-      sha256 "5d41efe31b3261fdaae2723ceee3673c12b03215bf7b45a66fae4dece8a45716"
+      url "https://registry-dev.pragma.com.co/repository/pragma-raw-dev-releases/pragma-ai-cli/0.1.4/pragma-ai-cli_0.1.4_darwin_amd64.tar.gz"
+      sha256 "2e223607373e017d7f883f4f79551b527ef859e09901bc532f558b9e641dfb24"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://registry-dev.pragma.com.co/repository/pragma-raw-dev-releases/pragma-ai-cli/0.1.3/pragma-ai-cli_0.1.3_linux_arm64.tar.gz"
-      sha256 "77fa650fdf7a8a731baecbf8a89e8400824230427a96e386aaf2156d91b16404"
+      url "https://registry-dev.pragma.com.co/repository/pragma-raw-dev-releases/pragma-ai-cli/0.1.4/pragma-ai-cli_0.1.4_linux_arm64.tar.gz"
+      sha256 "f91887fa4e3acb8939e9e00b933b5fdd50d7bf1863817c7732287dbc6932610f"
     end
     on_intel do
-      url "https://registry-dev.pragma.com.co/repository/pragma-raw-dev-releases/pragma-ai-cli/0.1.3/pragma-ai-cli_0.1.3_linux_amd64.tar.gz"
-      sha256 "89481bcf4a1620bc2afd2c40e58685eeedb3a5be6a3df474bd468382e60fdb59"
+      url "https://registry-dev.pragma.com.co/repository/pragma-raw-dev-releases/pragma-ai-cli/0.1.4/pragma-ai-cli_0.1.4_linux_amd64.tar.gz"
+      sha256 "aa6bc6d1d5588af52dfad80f97b040aaf12615776f96e270d5243c1d3ec30bc4"
     end
   end
 
@@ -39,7 +39,7 @@ class PragmaAi < Formula
     <<~EOS
       Pragma AI has been installed successfully.
 
-      Background services have been configured to sync your assets every 4 hours.
+      Background services have been configured to sync your assets every 24 hours.
 
       To get started, open a terminal and run:
         pragma-ai
